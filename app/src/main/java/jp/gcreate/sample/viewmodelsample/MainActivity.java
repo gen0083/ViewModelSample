951b78b0b604f8bde8353f43d8f7249437769314
Gen <archiherewego@gmail.com>
2018-09-25 21:10:24 +0900

    Add log on lifecycle event
    ViewはView自身でViewの状態を保存・復元する仕組みが実装されているので画面回転しても前の状態に戻る
    一方で、Activityで保持しているデータは、何もしなければデータが失われてしまう
    これはActivityのインスタンスが画面回転の前後で異なるからである

--- app/src/main/java/jp/gcreate/sample/viewmodelsample/MainActivity.java
+++ app/src/main/java/jp/gcreate/sample/viewmodelsample/MainActivity.java
@@ -20,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +30,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "test";
+    
     private EditText editText;
     private TextView textResult;
     private Button introductionButton;
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.editText);
@@ -95,5 +99,49 @@ public class MainActivity extends AppCompatActivity {
 
         user = new User();
         textResult.setText(user.introduction());
+
+        Log.d(TAG, "onCreate: " + editText.getText().toString());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: " + editText.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: " + editText.getText().toString());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: " + editText.getText().toString());
     }
 }
